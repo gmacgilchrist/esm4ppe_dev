12b900d45aecef8fa21d9c91d3faa0b600caee16
gmacgilchrist <graemem@princeton.edu>
2021-09-20 21:03:53 -0400

    notebook devising start year sampling strategy

---
+++ data/dmget_commands.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+root=/archive/oar.gfdl.cmip6/ESM4/DECK/ESM4_piControl_D/gfdl.ncrc4-intel16-prod-openmp/pp
+
+ppname=ocean_annual_rho2
+out=ts
+local=annual/5yr
+time=0[2-5]*
+var=vmo
+
+paths=${root}/${ppname}/${out}/${local}/${ppname}.${time}.${var}.nc
+
+dmget ${paths} &
---
+++ data/raw/CTRL/README.md
@@ -0,0 +1,3 @@
+#### Postprocess path
+/archive/oar.gfdl.cmip6/ESM4/DECK/ESM4_piControl_D/gfdl.ncrc4-intel16-prod-openmp/pp/
+
