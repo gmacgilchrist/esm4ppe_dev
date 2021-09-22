afcf109fd6341222bbd62445e69c1c4ed8c5b8c2
gmacgilchrist <graemem@princeton.edu>
2021-09-21 23:07:49 -0400

    calculates JSD for full ts and samples

--- data/dmget_commands.sh
+++ data/dmget_commands.sh
@@ -2,11 +2,11 @@
 
 root=/archive/oar.gfdl.cmip6/ESM4/DECK/ESM4_piControl_D/gfdl.ncrc4-intel16-prod-openmp/pp
 
-ppname=ocean_annual_rho2
+ppname=ocean_monthly
 out=ts
-local=annual/5yr
-time=0[2-5]*
-var=vmo
+local=monthly/5yr
+time=0[2-4]*
+var=sos
 
 paths=${root}/${ppname}/${out}/${local}/${ppname}.${time}.${var}.nc
 
