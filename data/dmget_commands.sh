85394e1a017e571f9e307435ebc6a9e721bdba0a
gmacgilchrist <graemem@princeton.edu>
2023-01-09 15:12:58 -0500

    updates github origin

--- data/dmget_commands.sh
+++ data/dmget_commands.sh
@@ -2,12 +2,11 @@
 
 root=/archive/oar.gfdl.cmip6/ESM4/DECK/ESM4_piControl_D/gfdl.ncrc4-intel16-prod-openmp/pp
 
-ppname=ocean_monthly
+ppname=ocean_annual_rho2
 out=ts
-local=monthly/5yr
-time=0[2-4]*
-var=sos
+local=annual/5yr
+time=01*
+var=vmo
 
 paths=${root}/${ppname}/${out}/${local}/${ppname}.${time}.${var}.nc
-
 dmget ${paths} &
